392d76a9049a00735af9947b326f207e72d3cd70
John Smith <example@stanford.edu>
2013-07-10 21:47:29 +0000

    Updated web.js to use index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
-
+var fs = require('fs');
+var buf = require('buffer');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var b = fs.readFileSync('index.html');
+    var s = b.toString;
+    response.send(s);
+  
 });
 
 var port = process.env.PORT || 5000;
